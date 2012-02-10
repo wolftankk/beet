d0875e51d2f639f6bec16ac18da916a6de5889c3
wolftankk <wolftankk@gmail.com>
2012-02-11 01:02:45 +0800

    重写了packages createPackageCategoryTree代码, 内部封装

--- js/beet/card/packages.js
+++ js/beet/card/packages.js
@@ -1,12 +1,289 @@
-function createPackageCategoryTree(isConfigMode){
+function createPackageCategoryTree(){
 	var me = this, cardServer = Beet.constants.cardServer, store;
+	var addWin, editWin;
 
-	var isConfigMode = (isConfigMode == undefined ? false : isConfigMode ); 
+	//private
+	var refreshTreeList = function(){
+		store.load();
+	}
+
+	var updateCategoryRate = function(widget, record, e){
+		var title = record.get("text"), id = record.get("id");
+		Ext.MessageBox.show({
+			title: "修改"+title+"打折率",
+			msg: "是否需要修改"+title+"打折率?",
+			buttons: Ext.MessageBox.YESNO,
+			fn: function(btn){
+				if (btn == "yes") {
+					Ext.MessageBox.prompt((title+"打折率"), "输入需要修改的打折率值:", function(btn, value, opts){
+						cardServer.UpdateCategoryRate(id, value, {
+							success: function(data){
+								if (data){
+									Ext.MessageBox.alert("通知", "修改成功!");
+									refreshTreeList();
+								}else{
+									Ext.MessageBox.alert("失败", "修改失败!");
+								}
+							},
+							failure: function(error){
+								Ext.Error.raise(error)
+							}
+						})
+					})
+				}
+			}
+		})
+	}
+
+
+	var categoryListCombo = function(){
+		var itemList = me.treeList.getStore().proxy.categoryList;
+		itemList.push({
+			id: -1,
+			text: "总分类"	
+		});
+		return Ext.create("Ext.data.Store", {
+			fields: ["id", "text"],
+			data: itemList
+		})
+	}
+
+	var addTreeItem = function(widget, record, e){
+		var CLCombo = categoryListCombo();
+		if (addWin){
+			addWin.close();
+		}
+		if (editWin){
+			editWin.close()
+		}
+
+		var form = Ext.create("Ext.form.Panel", {
+			width: "100%",
+			height: 100,
+			bodyStyle: "background-color: #dfe8f5",
+			border: false,
+			flex: 1,
+			bodyPadding: 10,
+			items: [
+				{
+					fieldLabel: "名称",
+					xtype: "textfield",
+					allowBlank: false,
+					name: "name"
+				},
+				{
+					fieldLabel: "所属类别",
+					xtype: "combobox",
+					store: CLCombo,
+					name: "parentid",
+					queryMode: "local",
+					displayField: "text",
+					valueField: "id",
+					value: parseInt(record.get("id") == "src" ? -1 : record.get("id"))
+				}
+			],
+			buttons: [
+				{
+					xtype: "button",
+					text: "提交",
+					width: 200,
+					handler: function(){
+						var f = form.getForm(), result = f.getValues();
+						result["categorytype"] = 2;
+						cardServer.AddCategory(Ext.JSON.encode(result), {
+							success: function(id){
+								if (id > 0){
+									Ext.Msg.alert("添加成功", "添加分类成功");
+									addWin.close();
+									refreshTreeList();
+								}
+							},
+							failure: function(error){
+								Ext.Error.raise(error)
+							}
+						})
+					}
+				}
+			]
+		});
+
+		addWin = Ext.create("Ext.window.Window", {
+			height: 140,
+			width: 300,
+			title: "增加分类",
+			autoHeight: true,
+			autoScroll: true,
+			autoWidth: true,
+		});
+		addWin.add(form)
+		addWin.doLayout();
+		addWin.show();
+	}
+	var deleteTreeItem = function(width, record, e){
+		var id = record.get("id");
+		if (id == "src"){
+			return;
+		}
+
+		Ext.Msg.alert("删除分类", "你确定需要删除 " + record.get("text") + " 吗?", function(btn){
+			cardServer.DeleteCategory(id, {
+				success: function(succ){
+					if (succ) {
+						Ext.Msg.alert("删除成功", "删除分类 "+ record.get("text") + " 成功");
+						refreshTreeList();
+					}
+				},
+				failure: function(error){
+					Ext.Error.raise(error)
+				}
+			});
+		})
+	}
+	var editTreeItem = function(widget, record, e){
+		var CLCombo = categoryListCombo();
+		if (addWin){
+			addWin.close();
+		}
+		if (editWin){
+			editWin.close()
+		}
+
+		var form = Ext.create("Ext.form.Panel", {
+			width: "100%",
+			height: 100,
+			bodyPadding: 10,
+			bodyStyle: "background-color: #dfe8f5",
+			border: false,
+			flex: 1,
+			items: [
+				{
+					fieldLabel: "名称",
+					xtype: "textfield",
+					allowBlank: false,
+					name: "name",
+					value: record.get("text")
+				},
+				{
+					fieldLabel: "所属类别",
+					xtype: "combobox",
+					store: CLCombo,
+					name: "parentid",
+					queryMode: "local",
+					displayField: "text",
+					valueField: "id",
+					value: parseInt(record.raw["pid"])
+				}
+			],
+			buttons: [
+				{
+					xtype: "button",
+					text: "提交",
+					width: 200,
+					handler: function(){
+						var f = form.getForm(), result = f.getValues();
+						result["id"] = record.get("id");
+						cardServer.UpdateCategory(Ext.JSON.encode(result), {
+							success: function(succ){
+								if (succ){
+									Ext.Msg.alert("编辑成功", "编辑分类成功");
+									editWin.close();
+									refreshTreeList();
+								}
+							},
+							failure: function(error){
+								Ext.Error.raise(error);
+							}
+						})
+					}
+				}
+			]
+		});
+
+		editWin= Ext.create("Ext.window.Window", {
+			height: 140,
+			width: 300,
+			title: "编辑分类",
+			autoHeight: true,
+			autoScroll: true,
+			autoWidth: true,
+		});
+		editWin.add(form)
+		editWin.doLayout();
+		editWin.show();
+	},
+
+	//private event handler
+	var treeListRClick = function(frame, record, item, index, e, options){
+		e.stopEvent();
+		var isLeaf = record.isLeaf(), me = this;
+		if (!record.contextMenu){
+			var menu = [];
+			//if (record.get("id") == "-1") { return false; }
+			if (record.isRoot()){
+				menu = [
+					{
+						text: "增加分类", 
+						handler: function(direction, e){
+							addTreeItem(direction, record, e)	
+						}
+					}
+				]
+			}else {
+				menu = [
+					{text: "增加分类", handler: function(direction, e){
+						addTreeItem(direction, record, e);	
+					}},
+					{text: "编辑", handler: function(direction, e){
+						editTreeItem(direction, record, e)	
+					}},
+					{text: "删除", handler: function(direction, e){
+						deleteTreeItem(direction, record, e);	
+					}},
+					{text: "修改打折率", handler: function(direction, e){
+						updateCategoryRate(direction, record, e);
+					}}
+				]
+			}
+
+			record.contextMenu = Ext.create("Ext.menu.Menu", {
+				style: {
+				   overflow: 'visible',
+				},
+				plain: true,
+				items: menu,
+				raw : record,
+				leaf: isLeaf
+			});
+		}
+		record.contextMenu.showAt(e.getXY());
+		return false;
+	}
+
+	var treeItemClick = function(frame, record, item, index, e, options){
+		if (!record){return;}
+		
+		me.selectProductCategoryId = parseInt(record.get("id"));
+
+		me.form.getForm().setValues({
+			"category" : record.get("text")	
+		})
+	}
+
+	///public API
+	me.onTreeItemClick = function(frame, record, item){
+		var id = record.get("id");
+		if (id != -1){
+			me.b_filter = "PCategoryId= " + id;
+		}else{
+			me.b_filter = "";
+		}
+		me.filterProducts();
+	}
 
 	me.createTreePanel = function(){
 		Ext.bind(buildCategoryTreeStore, me)(2);
 
-		me.storeProxy = store = Ext.create("Beet.apps.ProductsViewPort.PackagesCatgoryTreeStore");
+		store = Ext.create("Beet.apps.ProductsViewPort.PackagesCatgoryTreeStore");
 
 		//var sm = null;
 		//if (me.b_type == "selection"){
@@ -16,7 +293,7 @@ function createPackageCategoryTree(isConfigMode){
 		//	me.selModel = sm;
 		//}
 		me.treeList = Ext.create("Ext.tree.Panel", {
-			store: me.storeProxy,
+			store: store,
 			bodyStyle: "background-color: #fff",
 			frame: true,
 			lookMask: true,
@@ -67,6 +344,48 @@ function createPackageCategoryTree(isConfigMode){
 					dataIndex: 'rate'
 				}
 			],
+		});
+		me.treeList.addListener({
+			collapse: function(p){
+				if (p && p.collapsed && p.reExpander){
+					var reExpander = p.reExpander;
+					setTimeout(function(){
+						reExpander.el.applyStyles({top: 0, left: 0});
+						reExpander.setHeight(me.getHeight())
+					}, 50);
+				}
+			},
+			beforeitemcontextmenu: function(frame, record, item, index, e, options){
+				treeListRClick(frame, record, item, index, e, options);
+			},
+			itemclick: function(frame, record, item, index, e, options){
+				treeItemClick(frame, record, item, index, e, options);
+			}
+		});
+
+		me.treeList.storeProxy = me.treeList.getStore();
+	}
+}
+
+
+
+				//if (isConfigMode){
+				//	menu.concat([
+				//		"-",
+				//		{text: "查看详情", handler: function(direction, e){
+				//			me.onSelectItem(record.raw["id"], record)
+				//		}},
+				//		{text: "增加", handler: function(direction, e){
+				//			me.addPackageWindow();	
+				//		}},
+				//		{text: "删除", handler: function(direction, e){
+				//			me.deletePackage(record)
+				//		}},
+				//		{text: "修改打折率", handler: function(direction, e){
+				//			updateCategoryRate(direction, record, e);
+				//		}}
+				//	])
+				//}
 			//tbar: [
 			//	{
 			//		xtype: "button",
@@ -224,317 +543,6 @@ function createPackageCategoryTree(isConfigMode){
 			//		},
 			//	}
 			//]
-		});
-		me.treeList.addListener({
-			collapse: function(p){
-				if (p && p.collapsed && p.reExpander){
-					var reExpander = p.reExpander;
-					setTimeout(function(){
-						reExpander.el.applyStyles({top: 0, left: 0});
-						reExpander.setHeight(me.getHeight())
-					}, 50);
-				}
-			},
-			beforeitemcontextmenu: function(frame, record, item, index, e, options){
-				me.treeListRClick(frame, record, item, index, e, options);
-			},
-			itemclick: function(frame, record, item, index, e, options){
-				me.treeItemClick(frame, record, item, index, e, options);
-			}
-		});
-
-		me.treeList.storeProxy = me.treeList.getStore();
-	}
-
-	var updateCategoryRate = function(widget, record, e){
-		var title = record.get("text"), id = record.get("id");
-		Ext.MessageBox.show({
-			title: "修改"+title+"打折率",
-			msg: "是否需要修改"+title+"打折率?",
-			buttons: Ext.MessageBox.YESNO,
-			fn: function(btn){
-				if (btn == "yes") {
-					Ext.MessageBox.prompt((title+"打折率"), "输入需要修改的打折率值:", function(btn, value, opts){
-						cardServer.UpdateCategoryRate(id, value, {
-							success: function(data){
-								if (data){
-									Ext.MessageBox.alert("通知", "修改成功!");
-									me.refreshTreeList();
-								}else{
-									Ext.MessageBox.alert("失败", "修改失败!");
-								}
-							},
-							failure: function(error){
-								Ext.Error.raise(error)
-							}
-						})
-					})
-				}
-			}
-		})
-	}
-	me.refreshTreeList = function(){
-		me.treeList.storeProxy.load();
-	}
-
-	me.treeListRClick = function(frame, record, item, index, e, options){
-		e.stopEvent();
-		var isLeaf = record.isLeaf(), me = this;
-		if (!record.contextMenu){
-			var menu = [];
-			//if (record.get("id") == "-1") { return false; }
-			if (record.isRoot()){
-				menu = [
-					{
-						text: "增加分类", 
-						handler: function(direction, e){
-							me.addTreeItem(direction, record, e)	
-						}
-					}
-				]
-			}else {
-				menu = [
-					{text: "增加分类", handler: function(direction, e){
-						me.addTreeItem(direction, record, e);	
-					}},
-					{text: "编辑", handler: function(direction, e){
-						me.editTreeItem(direction, record, e)	
-					}},
-					{text: "删除", handler: function(direction, e){
-						me.deleteTreeItem(direction, record, e);	
-					}},
-					{text: "修改打折率", handler: function(direction, e){
-						updateCategoryRate(direction, record, e);
-					}}
-				]
-				//if (isConfigMode){
-				//	menu.concat([
-				//		"-",
-				//		{text: "查看详情", handler: function(direction, e){
-				//			me.onSelectItem(record.raw["id"], record)
-				//		}},
-				//		{text: "增加", handler: function(direction, e){
-				//			me.addPackageWindow();	
-				//		}},
-				//		{text: "删除", handler: function(direction, e){
-				//			me.deletePackage(record)
-				//		}},
-				//		{text: "修改打折率", handler: function(direction, e){
-				//			updateCategoryRate(direction, record, e);
-				//		}}
-				//	])
-				//}
-			}
-
-			record.contextMenu = Ext.create("Ext.menu.Menu", {
-				style: {
-				   overflow: 'visible',
-				},
-				plain: true,
-				items: menu,
-				raw : record,
-				leaf: isLeaf
-			});
-		}
-		record.contextMenu.showAt(e.getXY());
-		return false;
-	}
-
-	//////tree item
-	me.categoryListCombo = function(){
-		var itemList = me.treeList.getStore().proxy.categoryList;
-		itemList.push({
-			id: -1,
-			text: "总分类"	
-		});
-		return Ext.create("Ext.data.Store", {
-			fields: ["id", "text"],
-			data: itemList
-		})
-	}
-	me.addTreeItem = function(widget, record, e){
-		var CLCombo = me.categoryListCombo();
-		if (me.addWin){
-			me.addWin.close();
-		}
-		if (me.editWin){
-			me.editWin.close()
-		}
-		me.doLayout();
-
-		var form = Ext.create("Ext.form.Panel", {
-			width: "100%",
-			height: 100,
-			bodyStyle: "background-color: #dfe8f5",
-			border: false,
-			flex: 1,
-			bodyPadding: 10,
-			items: [
-				{
-					fieldLabel: "名称",
-					xtype: "textfield",
-					allowBlank: false,
-					name: "name"
-				},
-				{
-					fieldLabel: "所属类别",
-					xtype: "combobox",
-					store: CLCombo,
-					name: "parentid",
-					queryMode: "local",
-					displayField: "text",
-					valueField: "id",
-					value: parseInt(record.get("id") == "src" ? -1 : record.get("id"))
-				}
-			],
-			buttons: [
-				{
-					xtype: "button",
-					text: "提交",
-					width: 200,
-					handler: function(){
-						var f = form.getForm(), result = f.getValues();
-						result["categorytype"] = 2;
-						cardServer.AddCategory(Ext.JSON.encode(result), {
-							success: function(id){
-								if (id > 0){
-									Ext.Msg.alert("添加成功", "添加分类成功");
-									me.addWin.close();
-									me.refreshTreeList();
-								}
-							},
-							failure: function(error){
-								Ext.Error.raise(error)
-							}
-						})
-					}
-				}
-			]
-		});
-
-		me.addWin = Ext.create("Ext.window.Window", {
-			height: 140,
-			width: 300,
-			title: "增加分类",
-			autoHeight: true,
-			autoScroll: true,
-			autoWidth: true,
-		});
-		me.addWin.add(form)
-		me.addWin.doLayout();
-		me.addWin.show();
-	}
-	me.deleteTreeItem = function(width, record, e){
-		var id = record.get("id");
-		if (id == "src"){
-			return;
-		}
-
-		Ext.Msg.alert("删除分类", "你确定需要删除 " + record.get("text") + " 吗?", function(btn){
-			cardServer.DeleteCategory(id, {
-				success: function(succ){
-					if (succ) {
-						Ext.Msg.alert("删除成功", "删除分类 "+ record.get("text") + " 成功");
-						me.refreshTreeList();
-					}
-				},
-				failure: function(error){
-					Ext.Error.raise(error)
-				}
-			});
-		}, me)
-	}
-	me.editTreeItem = function(widget, record, e){
-		var CLCombo = me.categoryListCombo();
-		if (me.addWin){
-			me.addWin.close();
-		}
-		if (me.editWin){
-			me.editWin.close()
-		}
-
-		var form = Ext.create("Ext.form.Panel", {
-			width: "100%",
-			height: 100,
-			bodyPadding: 10,
-			bodyStyle: "background-color: #dfe8f5",
-			border: false,
-			flex: 1,
-			items: [
-				{
-					fieldLabel: "名称",
-					xtype: "textfield",
-					allowBlank: false,
-					name: "name",
-					value: record.get("text")
-				},
-				{
-					fieldLabel: "所属类别",
-					xtype: "combobox",
-					store: CLCombo,
-					name: "parentid",
-					queryMode: "local",
-					displayField: "text",
-					valueField: "id",
-					value: parseInt(record.raw["pid"])
-				}
-			],
-			buttons: [
-				{
-					xtype: "button",
-					text: "提交",
-					width: 200,
-					handler: function(){
-						var f = form.getForm(), result = f.getValues();
-						result["id"] = record.get("id");
-						cardServer.UpdateCategory(Ext.JSON.encode(result), {
-							success: function(succ){
-								if (succ){
-									Ext.Msg.alert("编辑成功", "编辑分类成功");
-									me.editWin.close();
-									me.refreshTreeList();
-								}
-							},
-							failure: function(error){
-								Ext.Error.raise(error);
-							}
-						})
-					}
-				}
-			]
-		});
-
-		me.editWin= Ext.create("Ext.window.Window", {
-			height: 140,
-			width: 300,
-			title: "编辑分类",
-			autoHeight: true,
-			autoScroll: true,
-			autoWidth: true,
-		});
-		me.editWin.add(form)
-		me.editWin.doLayout();
-		me.editWin.show();
-	},
-	me.onTreeItemClick = function(frame, record, item){
-		var id = record.get("id");
-		if (id != -1){
-			me.b_filter = "PCategoryId= " + id;
-		}else{
-			me.b_filter = "";
-		}
-		me.filterProducts();
-	}
-	me.treeItemClick = function(frame, record, item, index, e, options){
-		if (!record){return;}
-		
-		me.selectProductCategoryId = parseInt(record.get("id"));
-
-		me.form.getForm().setValues({
-			"category" : record.get("text")	
-		})
-	}
-}
 
 Ext.define("Beet.apps.ProductsViewPort.PackageList", {
 	extend: "Ext.panel.Panel",
