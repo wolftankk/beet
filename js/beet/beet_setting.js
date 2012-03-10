defb9af30cc442af1249f68ef79674aee09e21ca
wolftankk <wolftankk@gmail.com>
2012-03-10 20:59:38 +0800

    cleanup

--- js/beet/beet_setting.js
+++ js/beet/beet_setting.js
@@ -1,4 +1,4 @@
-Ext.namespace("Beet.settings")
+Ext.namespace("Beet.apps.settings")
 
 Ext.onReady(function(){
     Ext.syncRequire([
@@ -9,70 +9,7 @@ Ext.onReady(function(){
 	"settings.cards.charges",
 	"settings.cards.items",
 	"settings.cards.packages",
+	"settings.cards.rebate",
+	"settings.cards.interests"
     ])   
 })
-//registerBeetAppsMenu("configure", 
-//{
-//    title: "设置",
-//    items: [
-//        {
-//            xtype: "container",
-//            layout: "hbox",
-//            defaultType: "buttongroup",
-//            defaults: {
-//                height: 100,
-//                width: 250,
-//                autoWidth: true
-//            },
-//            items: [
-//                {
-//                    title: "产品管理",
-//                    width: 420,
-//                    columns: 6,
-//                    defaults: {
-//                        scale : "large"
-//                    },
-//                    autoWidth: true,
-//                    items: [
-//                        /*
-//                        {
-//                            text: "增加套餐",
-//                            handler: function(){
-//                                var item = Beet.apps.Menu.Tabs["addPackage"]
-//                                if (!item){
-//                                    Beet.workspace.addPanel("addPackage", "增加套餐", {
-//                                        items: [
-//                                            Ext.create("Beet.apps.ProductsViewPort.AddPackage")
-//                                        ]
-//                                    })
-//                                }else{
-//                                    Beet.workspace.workspace.setActiveTab(item);
-//                                }
-//                            }
-//                        },
-//                        */
-//                        {
-//                            text: "套餐设置",
-//                            handler: function(){
-//                                var item = Beet.apps.Menu.Tabs["packageList"]
-//                                if (!item){
-//                                    Beet.workspace.addPanel("packageList", "套餐设置", {
-//                                        items: [
-//                                            Ext.create("Beet.apps.ProductsViewPort.PackageList")
-//                                        ]
-//                                    })
-//                                }else{
-//                                    Beet.workspace.workspace.setActiveTab(item);
-//                                }
-//                            }
-//                        },
-//                    ]
-//                }
-//            ]
-//        }
-//    ]
-//});
-//
-//
-//Ext.namespace("Beet.apps.Viewport.Setting");
-//
