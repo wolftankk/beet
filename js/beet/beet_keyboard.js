24db717753b85dbc1c411fa8851546aa224c1f22
wolftankk <wolftankk@gmail.com>
2012-03-11 15:47:23 +0800

    增加屏蔽backspace退回上一页的功能

--- js/beet/beet_keyboard.js
+++ js/beet/beet_keyboard.js
@@ -1,3 +1,19 @@
 /**
  * 键盘快速绑定功能
  */
+
+
+//屏蔽BACKSPACE
+Ext.onReady(function(){
+    Ext.getBody().on("keydown", function(e){
+	if (e.getKey() == e.BACKSPACE){
+	    var target = e.getTarget();
+	    if ((target.readOnly == null || target.readOnly == undefined || target.readOnly == true) || (target.type != 'text' && target.type != 'textarea' && target.type != 'password')){
+		e.keyCode = 0
+		e.stopEvent();
+		e.stopPropagation();
+		return false;
+	    }
+	}
+    })
+})
