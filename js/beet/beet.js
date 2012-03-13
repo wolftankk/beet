c4e75c66dac4d9fd010f87920e6a65a2dad9d80d
wolftankk <wolftankk@gmail.com>
2012-03-13 23:49:05 +0800

    cleanup

--- js/beet/beet.js
+++ js/beet/beet.js
@@ -761,33 +761,3 @@ Ext.define("Beet.apps.Viewport", {
         }
     }
 });
-
-//获取服务项目
-Beet.apps.Viewport.getCustomerTypes = function(__callback){
-    if (Beet.cache.customerTypes == undefined){
-        Beet.cache.customerTypes = [];
-        Beet.constants.customerServer.GetCustomerTypes({
-            success: function(data){
-                var data = Ext.JSON.decode(data)
-                if (data["Data"] && Ext.isArray(data["Data"])){
-                    var datz = data["Data"];
-                    for (var item in datz){
-                        var p = datz[item];
-                        Beet.cache.customerTypes.push({
-                            boxLabel: p["TypeName"],
-                            name: "TypeName",
-                            inputValue: p["TypeGUID"]
-                        })
-                    }
-                }
-                __callback();
-            },
-            failure: function(){
-                Ext.Error.raise("与服务器断开链接");
-            }
-        })
-    }else{
-        __callback();
-    }
-}
-
