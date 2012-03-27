1a142b5c02403e93d531abd7503c784e34baadab
wolftankk <wolftankk@gmail.com>
2012-03-27 21:43:08 +0800

    update lib

--- js/my/MyCardLib_intf.js
+++ js/my/MyCardLib_intf.js
@@ -1141,6 +1141,40 @@ MyCardSvc.prototype.AddRebate = function(RebateJSON, __callback) {
   Beet_connection.asyncRequest("POST", this.url, __callbacks, Ext.JSON.encode(__message));
 }
 
+MyCardSvc.prototype.CancelConsumer = function(AIndexNo, __callback) {
+  var __message = {
+    "method" : "MyCardSvc.CancelConsumer",
+    "params" : {
+      "AIndexNo": AIndexNo
+      }
+  }
+  var __callbacks = null;
+  if (__callback) {
+    __callbacks = {
+      callback : __callback,
+      success : function (o) {
+        var __result = JSON.parse(o.responseText);
+        if (__result.error)
+        {
+          if ((typeof this.callback == "object") && this.callback.failure)
+            this.callback.failure(__result.error);
+        } else {
+          if ((typeof this.callback == "object") && this.callback.failure)
+            this.callback.success(__result.result);
+          else
+            this.callback(__result.result);
+        }
+      },
+      failure : function (o) {
+        if ((typeof this.callback == "object") && this.callback.failure) 
+            this.callback.failure(o);
+      },
+      timeout : 30000
+    }
+  }
+  Beet_connection.asyncRequest("POST", this.url, __callbacks, Ext.JSON.encode(__message));
+}
+
 MyCardSvc.prototype.DeleteCard = function(AID, __callback) {
   var __message = {
     "method" : "MyCardSvc.DeleteCard",
